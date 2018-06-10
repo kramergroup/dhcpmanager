55acf1db1af2750f27c40363306851b73a64d2fd
Denis Kramer <d.kramer@soton.ac.uk>
2018-06-10 18:58:06 +0100

    moved icon 3px down to better align with baseline

--- cmd/ui/frontend/src/StatusTableCell.js
+++ cmd/ui/frontend/src/StatusTableCell.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
 
 // Status: 0-Unbound, 1-Bound, 2-Stale, 3-Stopped
 const colors = ['#ffdb4d','#009900','#990000','#e6e6e6'];
 const stroke = 3;
 
+const styles = {
+  icon: {
+    transform: 'translate(0,3px)',
+  },
+};
+
 class StatusTableCell extends React.Component {
 
   render() {
+
+    const { classes } = this.props;
 
     var w = parseInt(this.props.size) + stroke;
     var h = parseInt(this.props.size) + stroke;
@@ -16,7 +25,7 @@ class StatusTableCell extends React.Component {
 
     return (
       <TableCell className={this.props.className}>
-        <svg width={w} height={h}>
+        <svg width={w} height={h} className={this.classes.icon}>
             <circle cx={w/2} cy={h/2} r={r} stroke={c} fill='transparent' stroke-width={stroke}/>
         </svg> 
       </TableCell>
@@ -24,6 +33,6 @@ class StatusTableCell extends React.Component {
 
   }
 
-}
+};
 
-export default StatusTableCell;
+export default withStyles(styles)(StatusTableCell);
